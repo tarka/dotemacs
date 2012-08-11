0cdc8e2335d21ef61f3b5faa7946deeed81c7d11
Steve Smith <tarkasteve@gmail.com>
2012-08-11 13:45:20 +1000

    Set default font

--- init.el
+++ init.el
@@ -95,5 +95,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "ProggySquareTTSZ" :foundry "unknown" :slant normal :weight normal :height 120 :width normal))))
+ '(default ((t (:inherit nil :stipple nil :background "DarkSlateGrey" :foreground "White" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 110 :width normal :foundry "apple" :family "ProggyClean"))))
  '(highlight ((t (:background "#244444")))))

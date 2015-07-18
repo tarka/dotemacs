6da6100ba9f4c6667e042505bf82bf530c3197cf
Steve Smith <ssmith@atlassian.com>
2015-07-18 18:38:18 +0200

    Change to Input Mono font

--- init.el
+++ init.el
@@ -284,5 +284,5 @@
    ;; If you edit it by hand, you could mess it up, so be careful.
    ;; Your init file should contain only one such instance.
    ;; If there is more than one, they won't work right.
-   `(default ((t (:inherit nil :stipple nil :background "DarkSlateGrey" :foreground "White" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 113 :width normal :foundry "unknown" :family ,proggy))))
+   '(default ((t (:inherit nil :stipple nil :background "DarkSlateGrey" :foreground "White" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 72 :width normal :foundry "unknown" :family "Input Mono"))))
    '(highlight ((t (:background "#244444"))))))

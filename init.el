e102a9a809d62bdbeede07f32604c185f7c44c33
Steve Smith <ssmith@atlassian.com>
2015-09-30 16:11:56 +0100

    Revert back to Proggy font

--- init.el
+++ init.el
@@ -320,6 +320,6 @@
    ;; If you edit it by hand, you could mess it up, so be careful.
    ;; Your init file should contain only one such instance.
    ;; If there is more than one, they won't work right.
-   '(default ((t (:inherit nil :stipple nil :background "DarkSlateGrey" :foreground "White" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 72 :width normal :foundry "unknown" :family "Input Mono"))))
+   `(default ((t (:inherit nil :stipple nil :background "DarkSlateGrey" :foreground "White" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 113 :width normal :foundry "unknown" :family ,proggy))))
    '(highlight ((t (:background "#244444"))))))
 

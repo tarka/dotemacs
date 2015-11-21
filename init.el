43cb778562b09ad251962d35d9f6441c74ac4bd6
Steve Smith <ssmith@atlassian.com>
2015-11-21 15:27:58 +0000

    Update Melpa URLs

--- init.el
+++ init.el
@@ -64,8 +64,8 @@
 (setq package-archives
       '(("gnu"          . "http://elpa.gnu.org/packages/")
         ("marmalade"    . "http://marmalade-repo.org/packages/")
-        ("melpa-stable" . "http://melpa-stable.milkbox.net/packages/")
-;        ("melpa"        . "http://melpa.milkbox.net/packages/")
+        ("melpa-stable" . "http://stable.melpa.org/packages/")
+        ;("melpa"        . "http://melpa.org/packages/")
         ("org"          . "http://orgmode.org/elpa/")))
 
 (setq package-enable-at-startup nil)

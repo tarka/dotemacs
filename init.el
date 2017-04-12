133c5222eaf294183baf2cfc81fab4c90d833e1f
Steve Smith <ssmith@atlassian.com>
2017-04-12 18:03:49 +1000

    Move custom setup to separate file

--- init.el
+++ init.el
@@ -1,3 +1,5 @@
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
 
 ;; Appearance and behaviour
 
@@ -64,6 +66,7 @@
 (setq-default indent-tabs-mode nil)
 (setq show-trailing-whitespace t)
 
+
 ;; Misc. editing/saving settings
 
 (setq default-directory (expand-file-name "~"))
@@ -101,6 +104,7 @@
                       projectile
                       flx-ido
                       clojure-mode
+                      clojure-mode-extra-font-locking
                       cider
                       clj-refactor
 		      magit
@@ -365,46 +369,4 @@
 (add-to-list 'auto-mode-alist '("\\.dockerfile\\'" . dockerfile-mode))
 
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(blink-cursor-mode nil)
- '(column-number-mode t)
- '(safe-local-variable-values
-   (quote
-    ((web-mode-content-types-alist
-      ("jsx" . ".*\\.js[x]?\\'"))
-     (eval add-to-list
-           (quote auto-mode-alist)
-           (quote
-            ("\\.js\\'" . web-mode)))
-     (whitespace-line-column . 80)
-     (lexical-binding . t))))
- '(show-paren-mode t)
- '(tool-bar-mode nil))
-
-(when (not (null window-system))
-  (setq proggy (if (eq window-system 'x) "ProggyCleanTT" "ProggyClean"))
-  (custom-set-faces
-   ;; custom-set-faces was added by Custom.
-   ;; If you edit it by hand, you could mess it up, so be careful.
-   ;; Your init file should contain only one such instance.
-   ;; If there is more than one, they won't work right.
-   `(default ((t (:inherit nil :stipple nil :background "DarkSlateGrey" :foreground "White" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 113 :width normal :foundry "unknown" :family ,proggy))))
-   '(highlight ((t (:background "#244444"))))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "DarkSlateGrey" :foreground "White" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 113 :width normal :foundry "unknown" :family "ProggyClean"))))
- '(company-scrollbar-bg ((t (:background "#1b2f2f"))))
- '(company-scrollbar-fg ((t (:background "#253f3f"))))
- '(company-tooltip ((t (:inherit default :background "#121f1f"))))
- '(company-tooltip-common ((t (:inherit font-lock-doc-face :background "DarkBlue"))))
- '(company-tooltip-selection ((t (:inherit font-lock-comment-face :background "MidnightBlue"))))
- '(highlight ((t (:background "#244444")))))
